447c430f53bb3449a0be91eb921ec4755097117c
Abdullah Waqar <abdullahwaqar332@gmail.com>
2025-01-31 22:00:14 -0700

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert into currency_exchange
+    (id, currency_from, currency_to, conversion_multiple, environment)
+VALUES
+    (10001,'USD','PKR',275,'');
+
+insert into currency_exchange
+(id, currency_from, currency_to, conversion_multiple, environment)
+VALUES
+    (10002,'CAD','PKR',197,'');
+
+insert into currency_exchange
+(id, currency_from, currency_to, conversion_multiple, environment)
+VALUES
+    (10003,'SAR','PKR',75,'');
