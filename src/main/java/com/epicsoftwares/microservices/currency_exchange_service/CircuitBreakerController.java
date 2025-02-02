9cf78c3e75e372dff2333d90399a5d19a127ddab
abdullah <abdullahwaqar332@gmail.com>
2025-02-02 00:34:10 -0700

    playing more with circuitbreaker

--- src/main/java/com/epicsoftwares/microservices/currency_exchange_service/CircuitBreakerController.java
+++ src/main/java/com/epicsoftwares/microservices/currency_exchange_service/CircuitBreakerController.java
@@ -1,5 +1,6 @@
 package com.epicsoftwares.microservices.currency_exchange_service;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,11 +15,16 @@ public class CircuitBreakerController {
     private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
     @GetMapping("/sample-api")
-    @Retry(name = "sample-api")
+//    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    @CircuitBreaker(name = "sample-api", fallbackMethod = "hardcodedResponse")
     public String sampleAPI() {
         logger.info("Sample API Called Received");
         ResponseEntity<String> entity =
                 new RestTemplate().getForEntity("http://localhost:8080/djoladfnhdf", String.class);
         return entity.getBody();
+    }
+
+    private String hardcodedResponse(Exception e) {
+        return "fallback-response";
     }
 }
